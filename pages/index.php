5af7207c9387fd6e210665466c42f699e755d544
Cedric Reed <cedricreed@Cedrics-MacBook-Air.local>
2025-07-15 05:37:13 -0700

    Add VITE_GBRANCH environment variable and display

--- pages/index.php
+++ pages/index.php
@@ -13,7 +13,7 @@ $read = true;
 
 	<div class="flex flex-col items-center">
 		<h3 class=" text-indigo-500"><?= VITE_NAME; ?></h3>
-		<h3 class=" text-green-500"><?= VITE_BRANCH; ?></h3>
+		<p class=" text-lg">Current Git Branch: <strong><?= VITE_BRANCH; ?></strong></p>
 
 		<?php if ($read): ?>
 			<p class=" mt-10">You have read <?= htmlspecialchars($book); ?>.</p>
