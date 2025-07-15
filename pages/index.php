48ac034ba71b1f997a2468af0d16d9f1799419af
Cedric Reed <cedricreed@Cedrics-MacBook-Air.local>
2025-07-15 15:52:14 -0700

    Add VITE_GBRANCH environment variable and display

--- pages/index.php
+++ pages/index.php
@@ -4,17 +4,22 @@ require('system/main.php');
 $layoutTemplate = new HTML('Modern PHP + Vite sethp')
 ?>
 
+<p class="text-lg absolute top-0">Current Git Branch: <strong><?= VITE_BRANCH; ?></strong></p>
+
 <div class="flex flex-col items-center gap-10 text-2xl">
 	<?php include('partials/nav.php'); ?>
 
 	<div class="flex flex-col items-center text-ye">
 		<?= VITE_NAME; ?>
-		<p class="text-lg">Current Git Branch: <strong><?= VITE_BRANCH; ?></strong></p>
+
+		<div>+</div>
+
+		<img src="%BASE%/logo.svg" class="w-20" />
 	</div>
 
 
 
-	<div id="repos" class="text-base flex gap-10"></div>
+	<!-- <div id="repos" class="text-base flex gap-10"></div> -->
 </div>
 
 <script src="/src/scripts/repos.ts" type="module"></script>
