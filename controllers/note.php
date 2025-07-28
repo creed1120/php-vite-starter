f0c1bd9c7ebf5d78b2899aacbaa96be499424568
Cedric Reed <cedricreed@Cedrics-MacBook-Air.local>
2025-07-27 17:29:56 -0700

    adding notes app

--- controllers/note.php
+++ controllers/note.php
@@ -1,1 +1,40 @@
 <?php
+/**
+ * Controller for the single /note route.
+ *
+ * This file handles the logic for the main entry point of the application.
+ * Add route handling, data processing, or view rendering as needed.
+ *
+ * @package php-vite-starter
+ * @author Cedric Reed
+ *
+ */
+
+require('system/main.php');
+// DATABASE CLASS LOADED IN THE "index.php" file
+// require('system/Database.php');
+
+$layoutTemplate = new HTML('Modern PHP + Vite sethp');
+$heading_name = "Note";
+
+$config = require('./config.php');
+
+$db = new Database($config['database']);
+// $query = "SELECT * FROM notes WHERE user_id = :id";
+$query = "SELECT * FROM notes WHERE id = :id";
+$note = $db->query($query, [
+        ':id' => $_GET['id']
+    ])->fetch();
+
+if( !$note ) {
+    abort();
+}
+
+$currentUserId = 1;
+
+if( $note['user_id'] !== $currentUserId ) {
+    abort(Response::FORBIDDEN);
+}
+
+// Load the View
+require('views/note.view.php');
