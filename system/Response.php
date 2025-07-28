a20f452b11583e1bcfbfb0150757e43551107b61
Cedric Reed <cedricreed@Cedrics-MacBook-Air.local>
2025-07-27 17:35:01 -0700

    adding notes app

---
+++ system/Response.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Class Response
+ *
+ * Represents an HTTP response with status code, headers, and body content.
+ *
+ * @package php-vite-starter
+ *
+ */
+
+class Response {
+
+    // Constants used to be more clear to developers and users
+    const NOT_FOUND = '404';
+    const FORBIDDEN = '403';
+}
